9db63be369a24985f0907ac840e6426b21ab4162
Paulinavegac <paulina.vegac@gmail.com>
2023-08-24 15:16:10 -0400

    UPDATE: styles and structure of the home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-ip';
+  title: string = 'IP';
+  getMyIP = () => {
+    console.log('MyIP')
+  }
 }
